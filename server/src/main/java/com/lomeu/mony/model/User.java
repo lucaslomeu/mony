b86654a1552aad7a5938241099107fe98fba61d6
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-03-25 14:36:25 -0300

    [FEAT] Entity User

---
+++ server/src/main/java/com/lomeu/mony/model/User.java
@@ -0,0 +1,23 @@
+package com.lomeu.mony.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
