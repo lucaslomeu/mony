e271ef686825aa8a6138c24765d53d6100309385
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-03-25 14:36:32 -0300

    [FEAT] Entity Subscription

---
+++ server/src/main/java/com/lomeu/mony/model/Subscription.java
@@ -0,0 +1,28 @@
+package com.lomeu.mony.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private LocalDate startDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
