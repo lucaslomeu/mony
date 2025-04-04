558d2b8f28cebc700eca2f29cbf6e3d508c55c4c
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-04-04 08:09:32 -0300

    [FEAT] [CLIENT] Generate account profile component

---
+++ client/src/app/features/account/account-profile/account-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-profile',
+  imports: [],
+  templateUrl: './account-profile.component.html',
+  styleUrl: './account-profile.component.scss'
+})
+export class AccountProfileComponent {
+
+}
---
+++ client/src/app/features/account/account.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AccountProfileComponent } from './account-profile/account-profile.component';
+
+export const ACCOUNT_ROUTES: Routes = [
+  { path: '', component: AccountProfileComponent },
+];
