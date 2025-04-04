e315824deba6678dfed5756a33223c2afa6d214f
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-04-04 08:10:26 -0300

    [FEAT] [CLIENT] Generate public layout

---
+++ client/src/app/layout/public-layout/public-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [RouterOutlet],
+  templateUrl: './public-layout.component.html',
+  styleUrl: './public-layout.component.scss',
+})
+export class PublicLayoutComponent {}
