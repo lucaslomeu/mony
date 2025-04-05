d81ce02a04b16ceef65c580ba256fc6f72cdb44e
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-04-05 12:18:53 -0300

    [FEAT] [CLIENT] Added Header/Footer Public Layout

---
+++ client/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
