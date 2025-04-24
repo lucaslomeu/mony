0cc2aea680e99b0bfe6494d185e10f0bee2639c0
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-04-24 09:25:21 -0300

    [FIX] Cors

--- server/src/main/java/com/lomeu/mony/security/SecurityConfig.java
+++ server/src/main/java/com/lomeu/mony/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(allowedOrigin));
+        config.setAllowedOrigins(List.of(allowedOrigin, "http://localhost:4200", "https://mony-lyart.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
